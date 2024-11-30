e36a924eeb040c8b8f6e71d7e7b9aa6507048fdd
Daniel <daniel.lam3987@gmail.com>
2024-11-29 19:36:51 -0500

    Typewriter implemented. Button being added.

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,32 +1,45 @@
-import React from 'React';
+import React from 'react';
+import Typewriter from "typewriter-effect";
 
-import { BackgroundBeams } from '@/components/ui/background-beams';
-//import { Button } from '@/components/ui/button';
-import { TypewriterEffectSmooth } from '@/components/ui/typewriter-effect';
+
 const Hero = () => {
 
     return (
         <div className='relative flex h-screen flex-col items-center justify-center bg-white'>
-            <div className='pointer-events-none absolute inset-0 flex items-center justify-center bg-white  [mask-image:radial-gradient(ellipse_at_center,transparent_50%,black)] dark:bg-black' />
-            <p className='text-s text-neutral-600 dark:text-neutral-200 sm:text-base'>
+            <div className='pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_50%,black)] dark:bg-black' />
+            <p className='text-s text-neutral-600 dark:text-neutral-200 sm:text-2xl'>
                 Hi, I'm Daniel. An aspiring developer.
             </p>
-            <TypewriterEffectSmooth options={{
-                strings: ['React.JS ', 'TailwindCSS ', 'Java '],
-                autoStart: true,
-                loop: false
-            }} />
-            <div className="z-10 flex flex-col items-center space-x-0 space-y-4 md: flex-row md:space-x-4 md: space-y-0">
-                <Link href="https://drive.google.com/file/d/1iO42FDTgDb96_FszY46Sdi9wTN9r8AsU/view?usp=sharing">
-                    <Button>Download CV</Button>
-                </Link>
-                <Link href="#contact-me">
-                    <Button variant="secondary">Contact Me</Button>
-                </Link>
+            <div>
+
+                <Typewriter
+                    repeatType="reverse"
+                    repeat="Infinity"
+                    onInit={(typewriter) => {
+                        typewriter
+                            .typeString("<span class='text-4xl'>I'm proficient in </span>")
+                            .pauseFor(250)
+                            .typeString("<span class='text-4xl font-bold text-blue-500'>Java</span>")
+                            .pauseFor(500)
+                            .deleteChars(4)
+                            .pauseFor(250)
+                            .typeString("<span class='text-4xl font-bold text-green-500'>TailWindCSS</span>")
+                            .pauseFor(500)
+                            .deleteChars(11)
+                            .pauseFor(250)
+                            .typeString("<span class='text-4xl font-bold text-red-500'>React</span>")
+                            .start();
+                    }}
+                />
+
             </div>
-            <BackgroundBeams />
+
+            
+            <div className="md: z-10 flex flex-row flex-col items-center space-x-0 space-y-0 space-y-4 md:space-x-4">
+                
+            </div>
         </div>
     );
 }
 
-export default Hero
+export default Hero;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,27 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const flattenColorPalette = require("tailwindcss/lib/util/flattenColorPalette").default;
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", 
+    "./public/index.html",      
+  ],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: [
+    function addVariablesForColors({ addBase, theme }) {
+      const allColors = flattenColorPalette(theme("colors"));
+      const newVars = Object.fromEntries(
+        Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+      );
+
+      addBase({
+        ":root": newVars,
+      });
+    },
+  ],
+};
